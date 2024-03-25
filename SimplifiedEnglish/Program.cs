31fb3f96849f72ff71bf5a3e24217312a2f459b4
vfrfv <valuev.12@mail.ru>
2024-03-25 15:55:03 +0300

    init

---
+++ SimplifiedEnglish/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplifiedEnglish
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int FindElementIndex(int[] array, int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == element)
+                    return i;
+
+            return -1;
+        }
+    }
+}
